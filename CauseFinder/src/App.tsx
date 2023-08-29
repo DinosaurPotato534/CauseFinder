8b3cf8620699259359789600be2977ba404b7841
DinosaurPotato534 <shaany0418@gmail.com>
2023-08-29 02:31:31 -0400

    d
    its 2:31 AM and I have 4 tests tomorrow.....

--- CauseFinder/src/App.tsx
+++ CauseFinder/src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Landing";
-import Search from "./pages/Search";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" Component={Home} />
-        <Route path="/Search" Component={Search} />
       </Routes>
     </Router>
   );
